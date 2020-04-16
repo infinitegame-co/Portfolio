225e266b62e67705b8e3e268ae2dc810b77f5a16
Irishmun <steefiscool70@gmail.com>
2020-04-16 14:29:08 +0200

    finished top bar
    added javascript for fullscreen

--- Poolside/wwwroot/js/Draggable.js
+++ Poolside/wwwroot/js/Draggable.js
@@ -1,42 +1,6 @@
-﻿dragElement(document.getElementById("Draggable"));
-
-function dragElement(elmnt) {
-    var PosX = 0, PosY = 0, OriginalPosX = 0, OriginalPosY = 0;
-    var ElmntHeader = document.getElementById(elmnt.id + "Header");
-    if (ElmntHeader) {
-        ElmntHeader.onmousedown = dragMouseDown;
-    }
-    else {
-        elmnt.onmousedown = dragMouseDown;
-    }
-
-    function dragMouseDown(e) {
-        e = e || window.event;
-        e.preventDefault();
-
-        OriginalPosX = e.clientX;
-        OriginalPosY = e.clientY;
-        document.onmouseup = closeDragElement;
-
-        document.onmousemove = elementDrag;
-    }
-
-    function elementDrag(e) {
-        e = e || window.event;
-        e.preventDefault();
-
-        PosX = OriginalPosX - e.clientX;
-        PosY = OriginalPosY - e.clientY;
-        OriginalPosX = e.clientX;
-        OriginalPosY = e.clientY;
-
-        elmnt.style.top = (elmnt.offsetTop - PosY) + "px";
-        elmnt.style.left = (elmnt.offsetLeft - PosX) + "px";
-    }
-
-    function closeDragElement() {
-
-        document.onmouseup = null;
-        document.onmousemove = null;
-    }
-}
+﻿$(function () {
+    $("#Draggable").draggable({
+        containment: "#containment-wrapper",
+        scroll: false
+    });
+});
---
+++ Poolside/wwwroot/js/Fullscreen.js
@@ -0,0 +1,27 @@
+﻿var elem = document.documentElement;
+function openFullscreen() {
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) {
+        elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) {
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) {
+        elem.msRequestFullscreen();
+    }
+    document.getElementById("Open").style.display = "none";
+    document.getElementById("Close").style.display = "inline-block";
+}
+function closeFullscreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) { /* Firefox */
+        document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE/Edge */
+        document.msExitFullscreen();
+    }
+    document.getElementById("Open").style.display = "inline-block";
+    document.getElementById("Close").style.display = "none";
+}
