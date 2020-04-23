2c819653696d92b68a340fcedce67b2dbd555b72
Irishmun <steefiscool70@gmail.com>
2020-04-23 15:57:02 +0200

    added multi-drag and window opening/closing

--- Poolside/wwwroot/js/Draggable.js
+++ Poolside/wwwroot/js/Draggable.js
@@ -1,6 +1,14 @@
-﻿$(function () {
-    $("#Draggable").draggable({
-        containment: "#containment-wrapper",
-        scroll: false
-    });
+﻿$('.Draggable-Container .Draggable').draggable({
+    containment: "#containment-wrapper",
+    multiple: false,
+    scroll: false,
+    selected: '.selected',
+    beforeStart: function () {
+        var $this = $(this);
+        if (!$this.hasClass('selected')) {
+            $this.siblings('.selected')
+                .removeClass('selected');
+            $this.addClass('selected');
+        }
+    }
 });
---
+++ Poolside/wwwroot/js/Windows.js
@@ -0,0 +1,14 @@
+﻿
+function CloseWindow(WindowId) {
+    var window = document.getElementById(WindowId).childNodes;
+    window[1].setAttribute('style', 'display: none;');
+}
+
+function OpenWindow(WindowId) {
+    var window = document.getElementById(WindowId).childNodes;
+        window[1].setAttribute('style', 'position: absolute; border: 1px solid #000; left:0; top:0; ');
+}
+function OpenWindowCoord(WindowId, x, y) {
+    var window = document.getElementById(WindowId).childNodes;
+        window[1].setAttribute('style', "position: absolute; border: 1px solid #000; left:" + x + "px;" + " top:" + y + "px;");
+}
